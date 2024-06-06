7a5a516b1990830be503cb7653c55db94dba4f75
nicole-iscte <nlnss@iscte-iul.pt>
2024-06-06 07:35:14 +0100

    ultimo gajpo

--- projeto/Assets/Estrutura de Dados/XMLReader.cs
+++ projeto/Assets/Estrutura de Dados/XMLReader.cs
@@ -33,9 +33,9 @@ public class XMLReader : MonoBehaviour
         string xmlContent = File.ReadAllText(xmlFilePath);
         //Debug.Log(xmlContent);
         string dtdContent = File.ReadAllText("Assets/Resources/dtd/play-out.dtd"); 
-        Debug.Log(dtdContent);
-
-        File.WriteAllText("Assets/Resources/dtd/tempXml.xml", xmlContent);
+        //Debug.Log(dtdContent);
+        
+        File.WriteAllText("Assets/Resources/dtd/tempXml.xml", xmlContent); // escreve o conteudo do xml num ficheiro temporario
 
         XmlReaderSettings settings = new XmlReaderSettings
         {
@@ -44,36 +44,36 @@ public class XMLReader : MonoBehaviour
             //IgnoreWhitespace = true // Isso instrui o XmlReader a ignorar nós de espaço em branco
         };
         Debug.Log(settings==null);
-        settings.ValidationEventHandler += new ValidationEventHandler(ValidationCallback);
+        settings.ValidationEventHandler += new ValidationEventHandler(ValidationCallback); //handler para validacao
 
-        XmlReader xmlr = XmlReader.Create("Assets/Resources/dtd/tempXml.xml", settings);
+        XmlReader xmlr = XmlReader.Create("Assets/Resources/dtd/tempXml.xml", settings); // leitor com as configuracoes especificadas
         BoardGameObject = boardGameObject;
         return ReadXML(xmlr, boardGameObject);  
            
     }
 
     private void ValidationCallback(object sender, ValidationEventArgs args) {
-    if (args.Severity == XmlSeverityType.Warning)
-        Debug.LogWarning("Warning: " + args.Message);
-    else if (args.Severity == XmlSeverityType.Error)
-        Debug.LogError("Error: " + args.Message);
-    }
+        if (args.Severity == XmlSeverityType.Warning)
+            Debug.LogWarning("Warning: " + args.Message);
+        else if (args.Severity == XmlSeverityType.Error)
+            Debug.LogError("Error: " + args.Message);
+        }
 
     public Game ReadXML(XmlReader xmlr, GameObject boardGameObject){
 
 
-        string game_name = ""; // Declare outside the switch statement
+        string game_name = ""; // nome do jogo
         Player[] roles = null;
         Board board = null;
         List<Unit[]> allTurns = new List<Unit[]>();
 
+        //le o xml ate ao final
         while(xmlr.Read()){
-            Debug.Log(xmlr.Name);
-            //switch case para escolher o que fazer com cada tag?? Se calhar ter uma função para cada tipo de tag, senão aqui fica too much
-            //todos eles vão ter de entrar dentro dos seus child -> pôr mais um loop nessas funções para percorrer filhos?
+            //Debug.Log(xmlr.Name);
 
             if (xmlr.NodeType == XmlNodeType.Element){
-                Debug.Log(xmlr.Name);
+                //Debug.Log(xmlr.Name);
+                // Decide o que fazer com base no nome do elemento e processa-o adequadamente
                 switch (xmlr.Name){
                     case "game": 
                         game_name = DealWithGame(xmlr); 
@@ -95,10 +95,12 @@ public class XMLReader : MonoBehaviour
             }
 
         }
+        // Cria um novo objeto Game com os dados lidos
         Game game = new Game(board, roles, allTurns, game_name); 
         return game;
     }
 
+    //retorna o nome do jogo
     string DealWithGame(XmlReader xmlr){
         return xmlr.GetAttribute("name");
     }
@@ -122,13 +124,14 @@ public class XMLReader : MonoBehaviour
 
 
     Board DealWithBoard(XmlReader xmlr, GameObject boardGameObject){
+        //le os atributos largura e altura
         int width = int.Parse(xmlr.GetAttribute("width"));
         int height = int.Parse(xmlr.GetAttribute("height"));
+        // cria uma matriz de tiles
         Tile[,] tiles = new Tile[width, height];
         int x = 0;
         int y = 0;
 
-        //ele estava a ler mal as tags, lia coisas vaizas potanto com isto só lê o que é suposto
         List<string> expectedTags = new List<string> { "village", "forest", "plain", "sea", "desert", "mountain" };
 
         while (xmlr.Read()) {
@@ -136,47 +139,47 @@ public class XMLReader : MonoBehaviour
                 //Debug.Log("Entrei");
                 // Verifica se a tag atual está na lista de tags esperadas
                 if (expectedTags.Contains(xmlr.Name)) {
-            switch (xmlr.Name) {
-                    case "village":
-                        tiles[x, y] = new Tile(TileType.Village);
-                        break;
-                    case "forest":
-                        tiles[x, y] = new Tile(TileType.Forest);
-                        break;
-                    case "plain":
-                        tiles[x, y] = new Tile(TileType.Plain);
-                        break;
-                    case "sea":
-                        tiles[x, y] = new Tile(TileType.Sea);
-                        break;
-                    case "desert":
-                        tiles[x, y] = new Tile(TileType.Desert);
-                        break;
-                    case "mountain":
-                        tiles[x, y] = new Tile(TileType.Mountain);
-                        break;
-                }
-                x++;
-                if (x >= width) {
-                    x = 0;
-                    y++;
-                }
-                if (y >= height) {
-                    break; // Termina o loop quando todos os elementos da matriz foram preenchidos
-                }
+                    switch (xmlr.Name) {
+                            case "village":
+                                tiles[x, y] = new Tile(TileType.Village);
+                                break;
+                            case "forest":
+                                tiles[x, y] = new Tile(TileType.Forest);
+                                break;
+                            case "plain":
+                                tiles[x, y] = new Tile(TileType.Plain);
+                                break;
+                            case "sea":
+                                tiles[x, y] = new Tile(TileType.Sea);
+                                break;
+                            case "desert":
+                                tiles[x, y] = new Tile(TileType.Desert);
+                                break;
+                            case "mountain":
+                                tiles[x, y] = new Tile(TileType.Mountain);
+                                break;
+                    }
+                    x++;
+                    if (x >= width) {
+                        x = 0;
+                        y++;
+                    }
+                    if (y >= height) {
+                        break; // Termina o loop quando todos os elementos da matriz foram preenchidos
+                    }
                 }
             }
         }
             //Debug.Log("Conteúdo da matriz tiles:");
-
+            /*
             for (int i = 0; i < width; i++)
             {
                 for (int j = 0; j < height; j++)
                 {
                     //Debug.Log("tiles[" + i + ", " + j + "]: " + tiles[i, j]);
                 }
-            }
-            Board board = new Board(width, height, tiles);
+            }*/
+            Board board = new Board(width, height, tiles); // Cria um novo objeto Board com as dimensões e a matriz de tiles
             board.InitializeTiles(boardGameObject);
             return board;
         }
@@ -184,12 +187,12 @@ public class XMLReader : MonoBehaviour
 
     List<Unit[]> DealWithTurns(XmlReader xmlr,Board board){
 
-        List<Unit[]> turnsList = new List<Unit[]>();
+        List<Unit[]> turnsList = new List<Unit[]>(); //lista de jogadas vazia
 
-            while (xmlr.ReadToFollowing("turn")){
-                List<Unit> unitsInTurn = new List<Unit>();
+            while (xmlr.ReadToFollowing("turn")){ // procura e le cada turn
+                List<Unit> unitsInTurn = new List<Unit>(); // cria uma lista para as units em casa turn
 
-                if (xmlr.ReadToDescendant("unit")){
+                if (xmlr.ReadToDescendant("unit")){ //le ate encontrar a unit
                     do{
                         int id = int.Parse(xmlr.GetAttribute("id"));
                         string role = xmlr.GetAttribute("role");
@@ -200,20 +203,20 @@ public class XMLReader : MonoBehaviour
 
                         Piece piece;
                         
-                        if (pieceDictionary.ContainsKey(id)) {
+                        if (pieceDictionary.ContainsKey(id)) { //verifica se a peca existe no dicionario
                             piece = pieceDictionary[id];
                         } else {
-                            piece = new Piece(id, new Player(role), type, x, y);
+                            piece = new Piece(id, new Player(role), type, x, y); //se nao adiciona a
                             pieceDictionary.Add(id, piece);
                         }
 
                         Unit unit = new Unit(action, piece, x, y, role);
-                        unitsInTurn.Add(unit);
-                    } while (xmlr.ReadToNextSibling("unit"));
+                        unitsInTurn.Add(unit); //cria e adiciona uma unit com os dados lidos
+                    } while (xmlr.ReadToNextSibling("unit")); //le ate haver units nessa turn
                 }
-                turnsList.Add(unitsInTurn.ToArray());
+                turnsList.Add(unitsInTurn.ToArray()); // adicona o array de units à lista de turns no turn atual
             }
-            return turnsList;
+            return turnsList; //devolve a lista de turns
     }
 
 }
